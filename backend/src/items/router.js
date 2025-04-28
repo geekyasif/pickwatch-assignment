b54cc283905e07f95b0c116637d403c32f71bf55
geekyasif <mohdasif.dev@gmail.com>
2025-04-28 06:55:57 +0530

    type[fix] removed unused code

--- backend/src/items/router.js
+++ backend/src/items/router.js
@@ -5,6 +5,5 @@ const router = new Router();
 router.get("/", ItemController.getItemList);
 router.get("/:id", ItemController.getItemDetails);
 router.delete("/:id", ItemController.deleteItem);
-router.post("/", ItemController.createItem);
 
 module.exports = router;
