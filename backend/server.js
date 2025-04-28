39bd65f6a389531a635e1da26b06a91748b8657c
geekyasif <mohdasif.dev@gmail.com>
2025-04-28 05:59:28 +0530

    type[fix] fixing cors and global error handler

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,29 @@
 const Koa = require("koa");
+const cors = require("@koa/cors");
 const app = new Koa();
 const routes = require("./src/routes");
 
+app.use(
+  cors({
+    origin(ctx) {
+      return ctx.get("Origin") || "*";
+    },
+  })
+);
+
 app.use(routes.routes(), routes.allowedMethods());
+
+app.use(async (ctx, next) => {
+  try {
+    await next();
+  } catch (err) {
+    ctx.status = err.status || 500;
+    console.error(err);
+    ctx.body = {
+      message: err.message,
+    };
+  }
+});
 
 app.listen(5001, () => {
   console.log("Server is running on port 5001");
